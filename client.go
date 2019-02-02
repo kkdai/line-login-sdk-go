88ff5ddb8f797d1e2ba79625d51869aef185f418
Evan <evanslin@gmail.com>
2019-02-03 00:32:14 +0800

    change client

--- client.go
+++ client.go
@@ -1,79 +1,131 @@
 package apiservice
 
 import (
-	"bytes"
+	"context"
 	"errors"
-	"fmt"
-	"io/ioutil"
-	"log"
+	"io"
 	"net/http"
+	"net/url"
+	"path"
 )
 
-type ErrorResponse struct {
-	Err       error
-	ErrorCode int
-}
-
+// APIEndpoint constants
 const (
-	StreamContent string = "application/octet-stream"
-	JsonContent   string = "application/json"
+	APIEndpointBase = "https://access.line.me"
+
+	APIEndpointAuthorize = "/oauth2/v2.1/authorize"
+	APIEndpointToken     = "/oauth2/v2.1/token"
 )
 
-//Client:
+// Client type
 type Client struct {
-	key string
+	channelSecret string
+	channelToken  string
+	endpointBase  *url.URL     // default APIEndpointBase
+	httpClient    *http.Client // default http.DefaultClient
 }
 
-//NewClient: New oxford client based on key
-func NewClient(key string) *Client {
-	c := new(Client)
-	c.key = key
+// ClientOption type
+type ClientOption func(*Client) error
 
-	return c
+// New returns a new bot client instance.
+func New(channelSecret, channelToken string, options ...ClientOption) (*Client, error) {
+	if channelSecret == "" {
+		return nil, errors.New("missing channel secret")
+	}
+	if channelToken == "" {
+		return nil, errors.New("missing channel access token")
+	}
+	c := &Client{
+		channelSecret: channelSecret,
+		channelToken:  channelToken,
+		httpClient:    http.DefaultClient,
+	}
+	for _, option := range options {
+		err := option(c)
+		if err != nil {
+			return nil, err
+		}
+	}
+	if c.endpointBase == nil {
+		u, err := url.ParseRequestURI(APIEndpointBase)
+		if err != nil {
+			return nil, err
+		}
+		c.endpointBase = u
+	}
+	return c, nil
 }
 
-//Connect :with API url and data, return response byte or error if http.Status is not OK
-func (c *Client) Connect(mode string, inUrl string, data *bytes.Buffer, useJson bool) ([]byte, *ErrorResponse) {
-	client := &http.Client{}
-	fmt.Println("connected: ", mode, inUrl, data)
-
-	r := &http.Request{}
-	if data == nil {
-		r, _ = http.NewRequest(mode, inUrl, nil)
-
-	} else {
-		r, _ = http.NewRequest(mode, inUrl, data)
+// WithHTTPClient function
+func WithHTTPClient(c *http.Client) ClientOption {
+	return func(client *Client) error {
+		client.httpClient = c
+		return nil
 	}
+}
 
-	if useJson {
-		r.Header.Add("Content-Type", JsonContent)
-	} else {
-		r.Header.Add("Content-Type", StreamContent)
+// WithEndpointBase function
+func WithEndpointBase(endpointBase string) ClientOption {
+	return func(client *Client) error {
+		u, err := url.ParseRequestURI(endpointBase)
+		if err != nil {
+			return err
+		}
+		client.endpointBase = u
+		return nil
 	}
+}
 
-	r.Header.Add("Ocp-Apim-Subscription-Key", c.key)
-	ret := new(ErrorResponse)
-	resp, err := client.Do(r)
+func (client *Client) url(endpoint string) string {
+	u := *client.endpointBase
+	u.Path = path.Join(u.Path, endpoint)
+	return u.String()
+}
 
+func (client *Client) do(ctx context.Context, req *http.Request) (*http.Response, error) {
+	req.Header.Set("Authorization", "Bearer "+client.channelToken)
+	req.Header.Set("User-Agent", "API-Service-Go/"+version)
+	if ctx != nil {
+		res, err := client.httpClient.Do(req.WithContext(ctx))
+		if err != nil {
+			select {
+			case <-ctx.Done():
+				err = ctx.Err()
+			default:
+			}
+		}
+
+		return res, err
+	}
+	return client.httpClient.Do(req)
+
+}
+
+func (client *Client) get(ctx context.Context, endpoint string, query url.Values) (*http.Response, error) {
+	req, err := http.NewRequest("GET", client.url(endpoint), nil)
 	if err != nil {
-		log.Println("er:", err)
-		ret.Err = err
-		return nil, ret
+		return nil, err
 	}
+	if query != nil {
+		req.URL.RawQuery = query.Encode()
+	}
+	return client.do(ctx, req)
+}
 
-	body, err := ioutil.ReadAll(resp.Body)
+func (client *Client) post(ctx context.Context, endpoint string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest("POST", client.url(endpoint), body)
 	if err != nil {
-		log.Println("er:", err)
-		ret.Err = err
-		return nil, ret
+		return nil, err
 	}
+	req.Header.Set("Content-Type", "application/json; charset=UTF-8")
+	return client.do(ctx, req)
+}
 
-	if resp.StatusCode > http.StatusAccepted {
-		ret.ErrorCode = resp.StatusCode
-		ret.Err = errors.New("Error on:" + string(body))
-		log.Println("Error happen! body:", string(body))
-		return body, ret
+func (client *Client) delete(ctx context.Context, endpoint string) (*http.Response, error) {
+	req, err := http.NewRequest("DELETE", client.url(endpoint), nil)
+	if err != nil {
+		return nil, err
 	}
-
-	return body, nil
+	return client.do(ctx, req)
 }
---
+++ version.go
@@ -0,0 +1,3 @@
+package apiservice
+
+const version = "1.0.0"
