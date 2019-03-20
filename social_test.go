628ace0ccfe81072003f337e9544544c27dfab30
Evan <evanslin@gmail.com>
2019-03-20 10:15:01 +0800

    Fix test with env

--- social_test.go
+++ social_test.go
@@ -6,19 +6,36 @@ import (
 	"log"
 	"net/http"
 	"net/url"
+	"os"
 	"strings"
 	"testing"
 )
 
-//Provide those data for testing.
-
-const (
-	accessToken  string = ""
-	cID          string = ""
-	cSecret      string = ""
-	qURL         string = ""
-	refreshToken string = ""
+var (
+	accessToken  string
+	refreshToken string
+	cID          string
+	cSecret      string
+	qURL         string
+	code         string
 )
+
+//Provide those data for testing.
+func init() {
+	accessToken = os.Getenv("LINE_ACCESS_TOKEN")
+	refreshToken = os.Getenv("LINE_REFRESH_TOKEN")
+	cID = os.Getenv("LINE_CLIENT_ID")
+	cSecret = os.Getenv("LINE_CLIENT_SECRET")
+	qURL = os.Getenv("LINE_SERVER_URL")
+	code = os.Getenv("LINE_LOGIN_CODE")
+}
+
+func checkEnvVariables(t *testing.T) {
+	if len(cID) == 0 || len(cSecret) == 0 {
+		log.Println("Please set environment variables for your LINE setting")
+		t.Skip("Please set environment variables for your LINE setting")
+	}
+}
 
 func RequestLoginToken(code, redirectURL, clientID, clientSecret string) (*TokenResponse, error) {
 	data := url.Values{}
@@ -60,7 +77,12 @@ func RequestLoginToken(code, redirectURL, clientID, clientSecret string) (*Token
 }
 
 func TestGetAccessToken(t *testing.T) {
-	code := "MhpPbGJMoPLQekqpKEX5"
+	checkEnvVariables(t)
+
+	if len(code) == 0 {
+		t.Skip("Skip it since don't exist LINE login code.")
+	}
+
 	client, _ := New(cID, cSecret)
 	ret, err := client.GetAccessToken(qURL, code).Do()
 	if err != nil {
@@ -71,6 +93,8 @@ func TestGetAccessToken(t *testing.T) {
 }
 
 func TestGetUserProfile(t *testing.T) {
+	checkEnvVariables(t)
+
 	client, _ := New(cID, cSecret)
 	ret, err := client.GetUserProfile(accessToken).Do()
 	if err != nil {
@@ -81,6 +105,8 @@ func TestGetUserProfile(t *testing.T) {
 }
 
 func TestGetURLCode(t *testing.T) {
+	checkEnvVariables(t)
+
 	scope := "profile openid" //profile | openid | email
 	state := GenerateNounce()
 	nounce := GenerateNounce()
@@ -91,6 +117,8 @@ func TestGetURLCode(t *testing.T) {
 }
 
 func TestVerifyToken(t *testing.T) {
+	checkEnvVariables(t)
+
 	client, _ := New(cID, cSecret)
 	ret, err := client.TokenVerify(accessToken).Do()
 	if err != nil {
@@ -101,6 +129,8 @@ func TestVerifyToken(t *testing.T) {
 }
 
 func TestRefreshToken(t *testing.T) {
+	checkEnvVariables(t)
+
 	client, _ := New(cID, cSecret)
 	ret, err := client.RefreshToken(refreshToken).Do()
 	if err != nil {
