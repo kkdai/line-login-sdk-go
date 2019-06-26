9376fcf96a6667b910d9677dfd21acc770369143
Evan <evanslin@gmail.com>
2019-06-26 21:53:50 +0800

    Update to latest parameters.

--- response.go
+++ response.go
@@ -34,14 +34,17 @@ type UserProfileResponse struct {
 }
 
 type Payload struct {
-	Iss     string `json:"iss"`
-	Sub     string `json:"sub"`
-	Aud     string `json:"aud"`
-	Exp     int    `json:"exp"`
-	Iat     int    `json:"iat"`
-	Nonce   string `json:"nonce"`
-	Name    string `json:"name"`
-	Picture string `json:"picture"`
+	Iss      string   `json:"iss"`
+	Sub      string   `json:"sub"`
+	Aud      string   `json:"aud"`
+	Exp      int      `json:"exp"`
+	Iat      int      `json:"iat"`
+	AuthTime int      `json:"auth_time"`
+	Nonce    string   `json:"nonce"`
+	Amr      []string `json:"amr"`
+	Name     string   `json:"name"`
+	Picture  string   `json:"picture"`
+	Email    string   `json:"email"`
 }
 
 // Token verification reponse
