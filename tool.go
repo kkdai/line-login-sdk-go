93bb3e01d94866a102a0d439b29168bc37f43e4a
Evan Lin <evanslin@gmail.com>
2021-04-14 11:15:12 +0800

    Prepare for PKCE

--- tool.go
+++ tool.go
@@ -1,16 +1,35 @@
 package social
 
 import (
+	"crypto/sha256"
 	b64 "encoding/base64"
 	"log"
 	"math/rand"
 	"time"
 )
 
-var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+var letterRunes = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._~")
 
 func init() {
 	rand.Seed(time.Now().UnixNano())
+}
+
+// PkceChallenge: base64-URL-encoded SHA256 hash of verifier, per rfc 7636
+func PkceChallenge(verifier string) string {
+	sum := sha256.Sum256([]byte(verifier))
+	challenge := b64.URLEncoding.WithPadding(b64.NoPadding).EncodeToString(sum[:])
+	return (challenge)
+}
+
+// GenerateCodeVerifier: Generate code verifier (length 43~128) for PKCE.
+func GenerateCodeVerifier(length int) string {
+	if length > 128 {
+		length = 128
+	}
+	if length < 43 {
+		length = 43
+	}
+	return b64.StdEncoding.EncodeToString([]byte(randStringRunes(length)))
 }
 
 func GenerateNonce() string {
